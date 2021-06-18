83bbccef658096e5fdd009e5853b1edbcb0e63bc
GitHub Action <action@github.com>
2021-06-18 15:06:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/longzhitengye/gokithelper/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/longzhitengye/gokithelper/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.longzhitengye.gokithelper.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.longzhitengye.gokithelper.MyBundle
 
 class MyApplicationService {
 
